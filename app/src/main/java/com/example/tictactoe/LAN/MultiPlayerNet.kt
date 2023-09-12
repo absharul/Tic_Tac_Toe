7160e3dac3b59c1ccebb3817df96b88499531997
Absharulhuda Sayed <absharulhuda@gmail.com>
2023-09-12 17:56:24 +0530

    ui chnages made

--- app/src/main/java/com/example/tictactoe/LAN/MultiPlayerNet.kt
+++ app/src/main/java/com/example/tictactoe/LAN/MultiPlayerNet.kt
@@ -6,8 +6,10 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.Window
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.Toast
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tictactoe.R
 import com.example.tictactoe.StartActiivity
@@ -16,6 +18,11 @@ import com.example.tictactoe.databinding.ActivityMultiPlayerNetBinding
 class MultiPlayerNet : AppCompatActivity() {
 
     private lateinit var binding: ActivityMultiPlayerNetBinding
+
+    private lateinit var listView: TextView
+    private lateinit var listView2: TextView
+    private lateinit var textList: MutableList<String>
+    private lateinit var adapter: ArrayAdapter<String>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,11 +33,22 @@ class MultiPlayerNet : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        listView = findViewById(R.id.list_contenthost)
+//        textList = mutableListOf()
+//        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, textList)
+//        listView.adapter = adapter
+
+        listView2 = findViewById(R.id.list_contentjoin)
+        textList = mutableListOf()
+//        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, textList)
+//        listView.adapter = adapter
+
+
         binding.btnHost.setOnClickListener {
-            showCustomeDialogBox()
+            showCustomeDialogBoxHost()
         }
         binding.btnJoin.setOnClickListener {
-            showCustomeDialogBox()
+            showCustomeDialogBoxJoin()
         }
 
         binding.btnBack.setOnClickListener {
@@ -38,20 +56,58 @@ class MultiPlayerNet : AppCompatActivity() {
             finish()
         }
 
+
+
     }
 
+    private fun showCustomeDialogBoxHost() {
 
-    private fun showCustomeDialogBox() {
+    val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.popup_inputhost)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+    val editText: EditText = dialog.findViewById(R.id.user_editText)
+
+
+
+    val btnDone: Button = dialog.findViewById(R.id.btnDone)
+
+        btnDone.setOnClickListener {
+//            Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show()
+            val text = editText.text.toString()
+            if (text.isNotEmpty()) {
+                listView.setText(text)
+//                adapter.notifyDataSetChanged()
+            }
+            dialog.dismiss()
+        }
+
+    dialog.show()
+    }
+
+    private fun showCustomeDialogBoxJoin() {
+        TODO("Not yet implemented")
         val dialog = Dialog(this)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setCancelable(false)
         dialog.setContentView(R.layout.popup_inputhost)
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
+        val editText: EditText = dialog.findViewById(R.id.user_editText)
+
+
+
         val btnDone: Button = dialog.findViewById(R.id.btnDone)
 
         btnDone.setOnClickListener {
-            Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show()
+//            Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show()
+            val text = editText.text.toString()
+            if (text.isNotEmpty()) {
+                listView2.setText(text)
+//                adapter.notifyDataSetChanged()
+            }
             dialog.dismiss()
         }
 
